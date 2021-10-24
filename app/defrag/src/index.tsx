c18ded0f9dfd504edc7f66bcca4047b8776b6ecf
Connor Mendenhall <cmendenhall@8thlight.com>
2021-10-24 02:45:48 +0100

    Change order of 98.css import

--- app/defrag/src/index.tsx
+++ app/defrag/src/index.tsx
@@ -1,13 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "98.css";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import config from "./config/dapp";
 
 import { DAppProvider } from "@usedapp/core";
-
-import "98.css";
 
 ReactDOM.render(
   <React.StrictMode>
